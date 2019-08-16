4997cfdd781de4de6714f013c6efe9b7e7094ede
kc1r74p <7422353+kc1r74p@users.noreply.github.com>
2019-08-16 21:50:30 +0200

    Create key_vals.sh

---
+++ key_vals.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+props="${ROOT_FOLDER}/keyval/keyval.properties"
+if [ -f "$props" ]
+then
+  echo "Reading passed key values"
+  while IFS= read -r var
+  do
+    if [ ! -z "$var" ]
+    then
+      echo "Adding: $var"
+      export "$var"
+    fi
+  done < "$props"
+fi
